8f856b13c82c49bbb84fa00752770861ea9a8932
Marouf Ahmed Thebo <mthebo164@gmail.com>
2024-04-01 21:11:38 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+// Asking Question through Inquirer
+const answers = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstname" },
+    { message: "Enter Second Number", type: "number", name: "secondName" },
+    {
+        message: "Select one Operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtractions", "Multiplication", "Division"],
+    },
+]);
+// Conditional Statement If-Else
+if (answers.operator === "Addition") {
+    console.log(answers.firstname + answers.secondName);
+}
+else if (answers.operator === "Subtractions") {
+    console.log(answers.firstname - answers.secondName);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstname * answers.secondName);
+}
+else if (answers.operator === "Division") {
+    console.log(answers.firstname / answers.secondName);
+}
+else {
+    console.log("Invalid Input");
+}
